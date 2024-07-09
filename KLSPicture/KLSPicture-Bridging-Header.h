ad2160678772ddfd094c594c1eabca3264d7a3bd
gwh <gwh199099@hotmail.com>
2024-07-09 08:54:39 +0800

    Initial commit

---
+++ KLSPicture/KLSPicture-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  KLSPicture-Bridging-Header.h
+//  KLSPicture
+//
+//  Created by gwh on 2024/7/9.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
